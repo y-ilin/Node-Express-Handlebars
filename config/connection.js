e4eefe87d9b1d58d5682249f1f6309e01fd0ddc6
Yi Lin Lee <yilinlee19@gmail.com>
2020-08-08 18:34:08 +1000

    hook up project to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
-});
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
